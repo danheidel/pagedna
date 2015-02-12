37d2e983006b72e1cb5f18a85efc47ddd55ed495
Dan Heidel <dan.heidel@gmail.com>
2015-02-11 17:57:30 -0800

    a proper directive-based check for input validation

--- site/static/js/directives.js
+++ site/static/js/directives.js
@@ -12,23 +12,31 @@ angular.module('pagednaApp.directives', [])
       if(!ngModel) return;
       if(!attribs.columns) return;
 
-      ngModel.$validators.validColumn = function(modelValue, viewValue){
+      ngModel.$validators.validName = function(modelValue, viewValue){
         var regex = /^(([A-FN-Z])(([-])((\d){1,4}))?)$/;
         var regexCheck = regex.test(modelValue);
-        scope.invalidColumnName = !regexCheck;
+        return regexCheck;
+      };
 
-        var duplicateCheck = true
+      ngModel.$validators.noDuplicates = function(modelValue, viewValue){
         var columns = scope[attribs.columns];
-        for(var rep=0;rep<columns.length;rep++){
-          if(columns[rep].name === modelValue){
+        return duplicateCheck(modelValue, columns);
+      };
+
+      function duplicateCheck(input, priorValues){
+        var duplicateCheck = true;
+        for(var rep=0;rep<priorValues.length;rep++){
+          if(priorValues[rep].name === input){
             duplicateCheck = false;
           }
         }
-
-        scope.duplicateColumnName = !duplicateCheck;
-
-        return regexCheck && duplicateCheck;
+        return duplicateCheck;
       }
+
+      scope.$watch(attribs.columns, function(newVal){
+        ngModel.$setValidity("noDuplicates",
+          duplicateCheck(ngModel.$modelValue, newVal));
+      }, true);
     }
   };
 });
